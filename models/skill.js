2aad4ea7e62199e03d9381de5df6dad8f18f4054
ndoelger <doelgern@gmail.com>
2023-06-16 09:36:35 -0400

    router.delete

--- models/skill.js
+++ models/skill.js
@@ -7,13 +7,29 @@ const skills = [
 module.exports = {
   getAll,
   getOne,
+  addOne,
+  deleteOne,
 };
 
 function getAll() {
+  console.log(skills);
   return skills;
 }
 
 function getOne(id) {
   id = parseInt(id);
   return skills.find((skill) => skill.id === id);
+}
+
+function addOne(skill) {
+  skill.id = Date.now() % 1000000;
+  skill.good = true;
+  skills.push(skill);
+  console.log(skills);
+}
+
+function deleteOne(id) {
+  id = parseInt(id);
+  const idx = skills.findIndex((todo) => todo.id === id);
+  skills.splice(idx, 1);
 }
--- routes/skills.js
+++ routes/skills.js
@@ -5,7 +5,15 @@ const skillsCtrl = require("../controllers/skills");
 // Get skills homepage
 router.get("/", skillsCtrl.index);
 
+//Get /skills/new
+router.get("/new", skillsCtrl.new);
+
 //Get /skills/:id
 router.get("/:id", skillsCtrl.show);
+
+//Post new skill
+router.post("/", skillsCtrl.create);
+
+router.delete("/:id", skillsCtrl.delete);
 
 module.exports = router;
